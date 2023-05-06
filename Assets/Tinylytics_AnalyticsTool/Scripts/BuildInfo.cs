a314a20c5328cd5410dbc84e1e725517c86b75ed
fourtap <35508083+fourtap@users.noreply.github.com>
2023-05-06 19:45:10 -0400

    fixed lighting

--- Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
+++ Assets/Tinylytics_AnalyticsTool/Scripts/BuildInfo.cs
@@ -1,1 +1,1 @@
-namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/6/2023 6:28:47 PM";}}
+namespace Tinylytics { public static class BuildInfo{public static string BUILD_TIME = "5/6/2023 7:38:33 PM";}}
